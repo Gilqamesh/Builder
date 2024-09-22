5f006969cc1b643c9a038f88a6a4f12de523cd50
Érchegyi Dávid <celicaw8@gmail.com>
2024-09-22 15:42:59 +0200

    Added: non_async_poc - audio nodes

---
+++ non_async_poc/audio_nodes.h
@@ -0,0 +1,27 @@
+#ifndef AUDIO_NODES_H
+# define AUDIO_NODES_H
+
+# include "node.h"
+
+struct sine_wave_node_t : public node_t {
+    struct sine_wave_t {
+        double frequency;
+        double amplitude;
+        double phase;
+    };
+    sine_wave_t wave_params;
+
+public:
+    sine_wave_node_t();
+};
+
+struct audio_mixer_node_t : public node_t {
+    static constexpr size_t sizeof_output_buffer = 4096;
+
+    thread mixer_thread;
+    bool mixer_thread_is_running = false;
+public:
+    audio_mixer_node_t();
+};
+
+#endif // AUDIO_NODES_H
