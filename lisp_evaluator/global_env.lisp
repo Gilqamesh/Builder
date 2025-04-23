1b6d4ba2409e8de2eadb5f3dd133d5b23959550c
gilqamesh <celicaw8@gmail.com>
2025-04-23 21:09:44 +0200

    add variadic parameters

--- lisp_evaluator/global_env.lisp
+++ lisp_evaluator/global_env.lisp
@@ -1,8 +1,26 @@
-(defmacro when (condition consequence)
-  (list 'if condition consequence))
+#| (defmacro when (condition consequence)
+  (list 'if condition consequence)) |#
 
 (define (map f l)
   (if (eq? l '())
       '()
       (cons (f (car l)) (map f (cdr l)))))
+
+(defmacro when (condition consequence)
+  `(if ,condition (begin ,@consequence)))
+
+
+
+#| (defmacro quasiquote (l)
+  (map (lambda (e) (if (list? e)
+                       (quasiquote e)
+                       ()
+       l)) |#
+
+(define (acc l r)
+  (if (nil? l)
+      r
+      (acc (cdr l) (+ (car l) r))))
+ 
+(define a ((lambda (x y . z) (+ x y (acc z 0))) 1 2 3 4 5 6 7 8 9 10))
 
