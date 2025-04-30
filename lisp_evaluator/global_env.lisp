b17e0b9e285cf454ec1d5ac0a07175ac50f00ff2
gilqamesh <celicaw8@gmail.com>
2025-04-30 22:57:07 +0200

    implement let* compile out voids for envs.. should move to cons cells only and remove non-atom special expr types that have ownership to other expr_t* fix and/or not evaluating its arguments

--- lisp_evaluator/global_env.lisp
+++ lisp_evaluator/global_env.lisp
@@ -8,36 +8,62 @@
       #f
       #t))
 
-(define (and expr . rest)
-  (if (not expr)
-      #f
-      (if (nil? rest)
-          #t
-          (apply and (cons (car rest) (cdr rest))))))
-
-(define (or expr . rest)
-  (if expr
-      #t
-      (if (nil? rest)
-          #f
-          (apply or (cons (car rest) (cdr rest)))))) 
-
 (define (map f l)
   (if (eq? l '())
       '()
       (cons (f (car l)) (map f (cdr l)))))
 
+(define (list-tail l n)
+  (if (= n 0)
+      l
+      (list-tail (cdr l) (- n 1))))
+
+(define (list-ref l n)
+  (car (list-tail l n)))
+
+#|
+  (let ((x 10) (y 20)) (+ x y)) -> ((lambda (x y) (+ x y)) 10 20)
+  (let f ((x 10) (y 20)) (f x y)) -> (let ((f (lambda (x y) (+ x y))))
+                                          (let ((x 10) (y 20))
+                                               (f x y)))
+                                  -> ((lambda (f) ((lambda (x y) (f x y)) 10 20)) (lambda (x y) (f x y)))
+(let fib-iter ((a 1)
+              (b 0)
+              (count n))
+     (if (= count 0)
+         b
+         (fib-iter (+ a b) a (- count 1))))
+(let ((fib-iter (lambda (a b count) 
+
+(let* ((a 10) (b (+ a 20)) (c (+ b a)))
+      (+ a b c))
+
+((lambda (a) ((lambda (b) ((lambda (c) (+ a b c)) (+ a 20))) (+ a 20))) 10)
+
+|#
 (defmacro let (var-val-pair-list . body-list)
   `((lambda ,(map (lambda (e) (car e)) var-val-pair-list) ,@body-list) ,@(map (lambda (e) (cadr e)) var-val-pair-list)))
+
+(defmacro let* (var-val-pair-list . body-list)
+  (define (let*-helper pair-list)
+    (if (and pair-list (cdr pair-list))
+        `((lambda (,(caar pair-list)) ,(let*-helper (cdr pair-list))) ,(list-ref (car pair-list) 1))
+        `((lambda (,(caar pair-list)) ,@body-list) ,(list-ref (car pair-list) 1))))
+  (let*-helper var-val-pair-list))
 
 (defmacro cond (. cond-cons-pairs)
   (define (cond-helper rem-cond-cons-pairs)
     (when rem-cond-cons-pairs
       (if (eq? (caar rem-cond-cons-pairs) 'else)
           `(begin ,@(cdar rem-cond-cons-pairs))
-          `(if ,(caar rem-cond-cons-pairs)
-               (begin ,@(cdar rem-cond-cons-pairs))
-               ,(cond-helper (cdr rem-cond-cons-pairs))))))
+          (if (eq? '=> (list-ref (car rem-cond-cons-pairs) 1)) #| Exercise 4.5 |#
+              (let ((cond-val (list-ref (car rem-cond-cons-pairs) 0)))
+                   `(if ,cond-val
+                        (apply ,(list-ref (car rem-cond-cons-pairs) 2) (,cond-val))
+                        ,(cond-helper (cdr rem-cond-cons-pairs))))
+              `(if ,(caar rem-cond-cons-pairs)
+                   (begin ,@(cdar rem-cond-cons-pairs))
+                   ,(cond-helper (cdr rem-cond-cons-pairs)))))))
   (cond-helper cond-cons-pairs))
 
 (defmacro when (condition . consequence)
