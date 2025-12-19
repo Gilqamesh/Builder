d323dfdc77350025fdff778d1e566dd28168efc0
gilqamesh <celicaw8@gmail.com>
2025-12-19 22:30:43 +0100

    squash me

--- builder_plugin.h
+++ builder_plugin.h
@@ -21,10 +21,6 @@ typedef struct builder_api_t {
 typedef void (*builder__build_self_t)(builder_ctx_t* ctx, const builder_api_t* api);
 typedef void (*builder__build_module_t)(builder_ctx_t* ctx, const builder_api_t* api, const char* static_libs);
 
-/**
- * Builds the module and installs its artifacts in the path provided in the c_module_t, it must produce API_LIB_NAME in the artifact_dir for dependent modules to use.
- * @param builder All pointers are only valid during the call and must not be retained.
-*/
 BUILDER_EXTERN void builder__build_self(builder_ctx_t* ctx, const builder_api_t* api);
 BUILDER_EXTERN void builder__build_module(builder_ctx_t* ctx, const builder_api_t* api, const char* static_libs);
 
